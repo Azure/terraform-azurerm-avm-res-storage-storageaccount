7f072baa8f63a4ae9dc280169a2b2320708d3733
hezijie <lonegunmanb@hotmail.com>
2023-07-19 16:02:03 +0800

    fix lint issue

--- test/upgrade/terraform_test.go
+++ test/upgrade/terraform_test.go
@@ -1,28 +1,28 @@
 package upgrade
 
 import (
-  "os"
-  "testing"
+	"os"
+	"testing"
 
-  test_helper "github.com/Azure/terraform-module-test-helper"
-  "github.com/gruntwork-io/terratest/modules/terraform"
+	test_helper "github.com/Azure/terraform-module-test-helper"
+	"github.com/gruntwork-io/terratest/modules/terraform"
 )
 
 func TestComplete(t *testing.T) {
-  currentRoot, err := test_helper.GetCurrentModuleRootPath()
-  if err != nil {
-    t.FailNow()
-  }
-  currentMajorVersion, err := test_helper.GetCurrentMajorVersionFromEnv()
-  if err != nil {
-    t.FailNow()
-  }
-  vars := make(map[string]any, 0)
-  identityId := os.Getenv("MSI_ID")
-  if identityId != "" {
-    vars["managed_identity_principal_id"] = identityId
-  }
-  test_helper.ModuleUpgradeTest(t, "Azure", "terraform-azurerm-storage-account", "examples/complete", currentRoot, terraform.Options{
-    Vars: vars,
-  }, currentMajorVersion)
+	currentRoot, err := test_helper.GetCurrentModuleRootPath()
+	if err != nil {
+		t.FailNow()
+	}
+	currentMajorVersion, err := test_helper.GetCurrentMajorVersionFromEnv()
+	if err != nil {
+		t.FailNow()
+	}
+	vars := make(map[string]any, 0)
+	identityId := os.Getenv("MSI_ID")
+	if identityId != "" {
+		vars["managed_identity_principal_id"] = identityId
+	}
+	test_helper.ModuleUpgradeTest(t, "Azure", "terraform-azurerm-storage-account", "examples/complete", currentRoot, terraform.Options{
+		Vars: vars,
+	}, currentMajorVersion)
 }
