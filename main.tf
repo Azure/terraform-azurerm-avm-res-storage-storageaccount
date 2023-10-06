0e7a7ddba3cceda8ae1ba6df70f6fe7b6bb0c294
chinthakaru <crupasinghe@microsoft.com>
2023-10-06 22:55:06 +0530

    Fix Ad verification issues

--- main.tf
+++ main.tf
@@ -601,7 +601,6 @@ resource "azurerm_role_assignment" "this" {
   role_definition_id                     = strcontains(lower(each.value.role_definition_id_or_name), lower(local.role_definition_resource_substring)) ? each.value.role_definition_id_or_name : null
   role_definition_name                   = strcontains(lower(each.value.role_definition_id_or_name), lower(local.role_definition_resource_substring)) ? null : each.value.role_definition_id_or_name
   principal_id                           = each.value.principal_id
-  #principal_type                         = each.value.principal_type
   condition                              = each.value.condition
   condition_version                      = each.value.condition_version
   skip_service_principal_aad_check       = each.value.skip_service_principal_aad_check
--- variables.tf
+++ variables.tf
@@ -928,7 +928,6 @@ variable "role_assignments" {
     skip_service_principal_aad_check       = optional(bool, false)
     condition                              = optional(string, null)
     condition_version                      = optional(string, null)
-    #principal_type                         = optional(string, null)
     delegated_managed_identity_resource_id = optional(string, null)
   }))
   default     = {}
