33d5cf818c887178d0e3b6e75cf922b81fc59545
Jason Leitch <jasonleitch0@gmail.com>
2023-11-11 17:33:17 +0000

    chore: remove duplicate log analytics block

--- examples/private_endpoint/main.tf
+++ examples/private_endpoint/main.tf
@@ -279,15 +279,3 @@ module "yet_another_container" {
     }
   }
 }
-
-resource "azurerm_log_analytics_storage_insights" "this" {
-  name                 = "storageinsight"
-  resource_group_name  = azurerm_resource_group.this.name
-  storage_account_id   = module.this.storage_account_id
-  storage_account_key  = module.this.storage_account_primary_access_key
-  workspace_id         = azurerm_log_analytics_workspace.this.id
-  blob_container_names = [for c in module.this.storage_container : c.name]
-  table_names          = [for t in module.this.storage_table : t.name]
-
-  depends_on = [module.this]
-}
