275668ad28392b619e1a08f419a48b2c90b87d6b
chinthakaru <crupasinghe@microsoft.com>
2024-01-25 14:09:22 +0530

    correct formatting

--- main.telemetry.tf
+++ main.telemetry.tf
@@ -7,8 +7,7 @@ resource "random_id" "telemetry" {
 # This is the module telemetry deployment that is only created if telemetry is enabled.
 # It is deployed to the resource's resource group.
 resource "azurerm_resource_group_template_deployment" "telemetry" {
-  count = var.enable_telemetry ? 1 : 0
-
+  count               = var.enable_telemetry ? 1 : 0
   deployment_mode     = "Incremental"
   name                = local.telem_arm_deployment_name
   resource_group_name = var.resource_group_name
