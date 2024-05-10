ef936b8023412344635cf21f254f51d84ba1874f
Rhys Ash <rhys.ash@hotmail.com>
2024-05-10 19:52:31 +0100

    add seperate variable for subresource_typs - #84

--- variables.tf
+++ variables.tf
@@ -109,7 +109,7 @@ variable "private_endpoints" {
     }), {})
     tags                                    = optional(map(any), null)
     subnet_resource_id                      = string
-    subresource_name                        = list(string)
+    subresource_names                       = list(string)
     private_dns_zone_group_name             = optional(string, "default")
     private_dns_zone_resource_ids           = optional(set(string), [])
     application_security_group_associations = optional(map(string), {})
@@ -121,6 +121,7 @@ variable "private_endpoints" {
     ip_configurations = optional(map(object({
       name               = string
       private_ip_address = string
+      subresource_name   = string
     })), {})
   }))
   default     = {}
@@ -132,7 +133,7 @@ A map of private endpoints to create on the resource. The map key is deliberatel
 - `lock` - (Optional) The lock level to apply to the private endpoint. Default is `None`. Possible values are `None`, `CanNotDelete`, and `ReadOnly`.
 - `tags` - (Optional) A mapping of tags to assign to the private endpoint.
 - `subnet_resource_id` - The resource ID of the subnet to deploy the private endpoint in.
-- `subresource_name` - The service name of the private endpoint.  Possible value are `blob`, 'dfs', 'file', `queue`, `table`, and `web`.
+- `subresource_names` - A list of service name of the private endpoint.  Possible value are `blob`, 'dfs', 'file', `queue`, `table`, and `web`.
 - `private_dns_zone_group_name` - (Optional) The name of the private DNS zone group. One will be generated if not set.
 - `private_dns_zone_resource_ids` - (Optional) A set of resource IDs of private DNS zones to associate with the private endpoint. If not set, no zone groups will be created and the private endpoint will not be associated with any private DNS zones. DNS records must be managed external to this module.
 - `application_security_group_resource_ids` - (Optional) A map of resource IDs of application security groups to associate with the private endpoint. The map key is deliberately arbitrary to avoid issues where map keys maybe unknown at plan time.
@@ -143,6 +144,7 @@ A map of private endpoints to create on the resource. The map key is deliberatel
 - `ip_configurations` - (Optional) A map of IP configurations to create on the private endpoint. If not specified the platform will create one. The map key is deliberately arbitrary to avoid issues where map keys maybe unknown at plan time.
   - `name` - The name of the IP configuration.
   - `private_ip_address` - The private IP address of the IP configuration.
+  - `subresource_name` -  The subresource this IP address applies to.
 DESCRIPTION
 }
 
