1d228e5f7f4958dc71cbbf3ae6686e2f1339bd60
chinthakaru <crupasinghe@microsoft.com>
2024-05-24 13:46:07 +0530

    update the role_assignments

--- examples/customer-managed-key/variables.tf
+++ examples/customer-managed-key/variables.tf
@@ -3,3 +3,9 @@ variable "bypass_ip_cidr" {
   default     = null
   description = "value to bypass the IP CIDR on firewall rules"
 }
+
+variable "msi_id" {
+  type        = string
+  description = "If you're running this example by authentication with identity, please set identity object id here."
+  default     = null
+}
