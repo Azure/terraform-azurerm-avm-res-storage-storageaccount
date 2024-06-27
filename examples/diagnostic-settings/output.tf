a51c72b064c739f55d46abb2f63a243f5d3aeaeb
chinthakaru <chinthakaru@gmail.com>
2024-06-27 22:58:51 +0530

    Updating EventHub resources

---
+++ examples/diagnostic-settings/output.tf
@@ -0,0 +1,26 @@
+output "eventhub_authorization_rule" {
+  value     = azurerm_eventhub_authorization_rule.this.primary_key
+  sensitive = true
+}
+output "shares" {
+  description = "value of shares"
+  value       = module.this.shares
+}
+output "queue" {
+  description = "value of queues"
+  value       = module.this.queues
+}
+output "tables" {
+  description = "value of tables"
+  value       = module.this.tables
+}
+output "containers" {
+  description = "value of containers"
+  value       = module.this.containers
+}
+
+output "resource" {
+  description = "value of storage_account"
+  value       = module.this.resource
+  sensitive   = true
+}
