87a1b95df11b9de84603d1838bdfca32efe27762
chinthakaru <crupasinghe@microsoft.com>
2024-07-04 17:05:59 +0530

    Removed hashicorp/time provider

--- terraform.tf
+++ terraform.tf
@@ -13,9 +13,5 @@ terraform {
       source  = "hashicorp/random"
       version = ">= 3.5.0, < 4.0.0"
     }
-    time = {
-      source  = "hashicorp/time"
-      version = ">= 0.9.1, < 2.0.0"
-    }
   }
 }
