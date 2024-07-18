ce0c2e18b704ce9f8fb5c85ad512c336c1142ad7
chinthakaru <chinthakaru@gmail.com>
2024-07-18 22:22:19 +0530

    Update variables according to Well-Architected Framework (#130)
    Update the following default values 
    
    1. versioning_enabled - true
    2. delete_retention_policy - 7 days
    3. container_delete_retention_policy - 7 days

--- examples/default/main.tf
+++ examples/default/main.tf
@@ -166,9 +166,7 @@ module "this" {
     blob_container1 = {
       name                  = "blob-container-${random_string.this.result}-1"
       container_access_type = "private"
-      immutable_storage_with_versioning = {
-        enabled = true
-      }
+
     }
 
   }
