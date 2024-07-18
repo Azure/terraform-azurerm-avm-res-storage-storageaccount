bc6a0aa469b59a3a43e9377bc3daea8f7fc5f4fc
chinthakaru <chinthakaru@gmail.com>
2024-07-18 17:29:32 +0530

    Update WAF default values

---
+++ examples/waf/output.tf
@@ -0,0 +1,23 @@
+output "shares" {
+  description = "value of shares"
+  value       = module.this.shares
+}
+
+output "queue" {
+  description = "value of queues"
+  value       = module.this.queues
+}
+output "tables" {
+  description = "value of tables"
+  value       = module.this.tables
+}
+output "containers" {
+  description = "value of containers"
+  value       = module.this.containers
+}
+
+output "resource" {
+  description = "value of storage_account"
+  value       = module.this.resource
+  sensitive   = true
+}
