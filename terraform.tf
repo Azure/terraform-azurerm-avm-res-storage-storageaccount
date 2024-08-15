b9c8dedb6d35ba8df97cdaf11db91492706f9254
chinthakaru <chinthakaru@gmail.com>
2024-08-15 09:47:30 +0530

    Updating minimum versions of AzApi and AzureRM Provider

--- terraform.tf
+++ terraform.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     azapi = {
       source  = "Azure/azapi"
-      version = ">= 1.9.0, < 2.0.0"
+      version = ">= 1.14.0, < 2.0.0"
     }
     azurerm = {
       source  = "hashicorp/azurerm"
