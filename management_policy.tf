e674087ccc949d6d865c4fa154f3872925d26f5b
chinthakaru <chinthakaru@gmail.com>
2024-08-27 22:03:44 +0530

    Updating the azurerm_storage_management_policy

--- management_policy.tf
+++ management_policy.tf
@@ -1,5 +1,4 @@
 resource "azurerm_storage_management_policy" "this" {
-  #   count = var.storage_management_policy_rule == null ? 0 : 1
   for_each = var.storage_management_policy_rule
 
   storage_account_id = azurerm_storage_account.this.id
