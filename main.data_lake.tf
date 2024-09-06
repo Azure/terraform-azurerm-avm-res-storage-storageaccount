45037adf6e60fc29f40dc3c20edd2efb03b763f2
chinthakaru <chinthakaru@gmail.com>
2024-09-06 22:22:07 +0530

    Initial commit

--- main.data_lake.tf
+++ main.data_lake.tf
@@ -1,14 +1,14 @@
 resource "azurerm_storage_data_lake_gen2_filesystem" "this" {
-  count = var.storage_data_lake_gen2_filesystem_name != null ? 1 : 0
-  name                     = var.storage_data_lake_gen2_filesystem_name
-  storage_account_id       = var.storage_data_lake_gen2_filesystem_storage_account_id
-  default_encryption_scope = var.storage_data_lake_gen2_filesystem_default_encryption_scope
-  group                    = var.storage_data_lake_gen2_filesystem_group
-  owner                    = var.storage_data_lake_gen2_filesystem_owner
-  properties               = var.storage_data_lake_gen2_filesystem_properties
+  count = var.storage_data_lake_gen2_filesystem != null ? 1 : 0
+  name                     = var.storage_data_lake_gen2_filesystem.name
+  storage_account_id       = var.storage_data_lake_gen2_filesystem.storage_account_id
+  default_encryption_scope = var.storage_data_lake_gen2_filesystem.default_encryption_scope
+  group                    = var.storage_data_lake_gen2_filesystem.group
+  owner                    = var.storage_data_lake_gen2_filesystem.owner
+  properties               = var.storage_data_lake_gen2_filesystem.properties
 
   dynamic "ace" {
-    for_each = var.storage_data_lake_gen2_filesystem_ace == null ? [] : var.storage_data_lake_gen2_filesystem_ace
+    for_each = var.storage_data_lake_gen2_filesystem.ace== null ? [] : var.storage_data_lake_gen2_filesystem.ace
     content {
       permissions = ace.value.permissions
       type        = ace.value.type
@@ -17,7 +17,7 @@ resource "azurerm_storage_data_lake_gen2_filesystem" "this" {
     }
   }
   dynamic "timeouts" {
-    for_each = var.storage_data_lake_gen2_filesystem_timeouts == null ? [] : [var.storage_data_lake_gen2_filesystem_timeouts]
+    for_each = var.storage_data_lake_gen2_filesystem.timeouts == null ? [] : [var.storage_data_lake_gen2_filesystem.timeouts]
     content {
       create = timeouts.value.create
       delete = timeouts.value.delete
