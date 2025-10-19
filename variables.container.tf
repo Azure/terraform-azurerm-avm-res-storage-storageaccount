41cd226e9a2dee40213b8b0ab2d3763eb6bd791a
chinthakaru <chinthakaru@gmail.com>
2025-10-19 23:13:47 +0530

    Remove unwanted container_access_type

--- variables.container.tf
+++ variables.container.tf
@@ -112,7 +112,6 @@ variable "containers" {
   }))
   default     = {}
   description = <<-EOT
- - `container_access_type` - (Optional) The Access Level configured for this Container. Possible values are `Blob`, `Container` or `None`. Defaults to `None`.
  - `metadata` - (Optional) A mapping of MetaData for this Container. All metadata keys should be lowercase.
  - `name` - (Required) The name of the Container which should be created within the Storage Account. Changing this forces a new resource to be created.
 
