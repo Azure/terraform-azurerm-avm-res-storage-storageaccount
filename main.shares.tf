675f61a955e26a57a4a55e2fd2f321e527dbd639
chinthakaru <chinthakaru@gmail.com>
2025-10-20 18:57:23 +0530

    pre-commit

--- main.shares.tf
+++ main.shares.tf
@@ -7,7 +7,7 @@ resource "azapi_resource" "share" {
   body = {
     properties = {
       metadata          = each.value.metadata
-      accessTier        = each.value.access_tier
+      accesstier        = each.value.access_tier
       enabledProtocols  = each.value.enabled_protocol
       shareQuota        = each.value.quota
       rootSquash        = each.value.root_squash
