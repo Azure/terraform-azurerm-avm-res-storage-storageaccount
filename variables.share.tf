55d45f0d1d1d4cec4fa0f0f46618e2f4a760f754
chinthakaru <chinthakaru@gmail.com>
2025-11-05 08:12:52 +0530

    Fixing Azure files vars

--- variables.share.tf
+++ variables.share.tf
@@ -21,10 +21,10 @@ variable "azure_files_authentication" {
  `active_directory` block supports the following:
  - `domain_guid` - (Required) Specifies the domain GUID.
  - `domain_name` - (Required) Specifies the primary domain that the AD DNS server is authoritative for.
- - `domain_sid` - (Required) Specifies the security identifier (SID).
- - `forest_name` - (Required) Specifies the Active Directory forest.
- - `netbios_domain_name` - (Required) Specifies the NetBIOS domain name.
- - `storage_sid` - (Required) Specifies the security identifier (SID) for Azure Storage.
+ - `domain_sid` - (Optional) Specifies the security identifier (SID).
+ - `forest_name` - (Optional) Specifies the Active Directory forest.
+ - `netbios_domain_name` - (Optional) Specifies the NetBIOS domain name.
+ - `storage_sid` - (Optional) Specifies the security identifier (SID) for Azure Storage.
 EOT
 }
 
