62662dbfc99d3ad964d8e409412a3b06157ce90c
chinthakaru <chinthakaru@gmail.com>
2025-11-06 08:36:32 +0530

    Add azurerm_storage_data_lake_gen2_path to Data Lake

--- main.data_lake.tf
+++ main.data_lake.tf
@@ -44,3 +44,47 @@ resource "azurerm_storage_data_lake_gen2_filesystem" "this" {
   depends_on = [azurerm_storage_account.this]
 }
 
+# Wait for role assignments to propagate before creating paths with ACLs
+resource "time_sleep" "wait_for_rbac" {
+  create_duration = "60s"
+
+  depends_on = [azurerm_role_assignment.storage_account]
+}
+
+resource "azurerm_storage_data_lake_gen2_path" "this" {
+  for_each = var.storage_data_lake_gen2_paths == null ? {} : var.storage_data_lake_gen2_paths
+
+  filesystem_name    = each.value.filesystem_name
+  path               = each.value.path
+  resource           = each.value.resource
+  storage_account_id = azurerm_storage_account.this.id
+  group              = each.value.group
+  owner              = each.value.owner
+
+  dynamic "ace" {
+    for_each = each.value.ace == null ? [] : each.value.ace
+
+    content {
+      permissions = ace.value.permissions
+      type        = ace.value.type
+      id          = ace.value.id
+      scope       = ace.value.scope
+    }
+  }
+  dynamic "timeouts" {
+    for_each = each.value.timeouts == null ? [] : [each.value.timeouts]
+
+    content {
+      create = timeouts.value.create
+      delete = timeouts.value.delete
+      read   = timeouts.value.read
+      update = timeouts.value.update
+    }
+  }
+
+  depends_on = [
+    azurerm_storage_data_lake_gen2_filesystem.this,
+    azurerm_role_assignment.storage_account,
+    time_sleep.wait_for_rbac
+  ]
+}
--- variables.data_lake.tf
+++ variables.data_lake.tf
@@ -45,3 +45,50 @@ EOT
     error_message = "Key `legacy` is preserved for backward compatibility."
   }
 }
+
+variable "storage_data_lake_gen2_paths" {
+  type = map(object({
+    path               = string
+    filesystem_name    = string
+    resource           = string
+    owner              = optional(string)
+    group              = optional(string)
+    ace = optional(set(object({
+      id          = optional(string)
+      permissions = string
+      scope       = optional(string)
+      type        = string
+    })))
+    timeouts = optional(object({
+      create = optional(string)
+      delete = optional(string)
+      read   = optional(string)
+      update = optional(string)
+    }))
+  }))
+  default     = {}
+  description = <<-EOT
+ - `path` - (Required) The path which should be created within the Data Lake Gen2 File System in the Storage Account. Changing this forces a new resource to be created.
+ - `filesystem_name` - (Required) The name of the Data Lake Gen2 File System which should be created within the Storage Account. Changing this forces a new resource to be created.
+ - `storage_account_id` - (Required) The ID of the Storage Account where the Data Lake Gen2 File System exists. Changing this forces a new resource to be created.
+ - `resource` - (Required) The type of resource. Possible values are `directory` or `file`. Changing this forces a new resource to be created.
+ - `owner` - (Optional) Specifies the Object ID of the Azure Active Directory User to make the owning user. Possible values also include `$superuser`.
+ - `group` - (Optional) Specifies the Object ID of the Azure Active Directory Group to make the owning group. Possible values also include `$superuser`.
+
+ ---
+ `ace` block supports the following:
+ - `id` - (Optional) Specifies the Object ID of the Azure Active Directory User or Group that the entry relates to. Only valid for `user` or `group` entries.
+ - `permissions` - (Required) Specifies the permissions for the entry in `rwx` form. For example, `rwx` gives full permissions but `r--` only gives read permissions.
+ - `scope` - (Optional) Specifies whether the ACE represents an `access` entry or a `default` entry. Default value is `access`.
+ - `type` - (Required) Specifies the type of entry. Can be `user`, `group`, `mask` or `other`.
+
+ ---
+ `timeouts` block supports the following:
+ - `create` - (Defaults to 30 minutes) Used when creating the Data Lake Gen2 Path.
+ - `delete` - (Defaults to 30 minutes) Used when deleting the Data Lake Gen2 Path.
+ - `read` - (Defaults to 5 minutes) Used when retrieving the Data Lake Gen2 Path.
+ - `update` - (Defaults to 30 minutes) Used when updating the Data Lake Gen2 Path.
+EOT
+}
+
+
