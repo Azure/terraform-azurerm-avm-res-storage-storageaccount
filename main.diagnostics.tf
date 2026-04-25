28c2f325203dbdaf7f10245c5cedc146810764ef
chinthakaru <chinthakaru@gmail.com>
2026-04-25 22:06:12 +0530

    fix: add missing properties to diagnostic settings for storage account, blob, queue, table, and azure files

--- main.diagnostics.tf
+++ main.diagnostics.tf
@@ -8,6 +8,9 @@ resource "azurerm_monitor_diagnostic_setting" "storage_account" {
   eventhub_authorization_rule_id = each.value.event_hub_authorization_rule_resource_id
   eventhub_name                  = each.value.event_hub_name
   log_analytics_workspace_id     = each.value.workspace_resource_id
+  log_analytics_destination_type = each.value.log_analytics_destination_type
+  storage_account_id             = each.value.storage_account_resource_id
+  partner_solution_id            = each.value.marketplace_partner_resource_id
 
   dynamic "enabled_metric" {
     for_each = each.value.metric_categories != null ? each.value.metric_categories : []
@@ -26,6 +29,9 @@ resource "azurerm_monitor_diagnostic_setting" "blob" {
   eventhub_authorization_rule_id = each.value.event_hub_authorization_rule_resource_id
   eventhub_name                  = each.value.event_hub_name
   log_analytics_workspace_id     = each.value.workspace_resource_id
+  log_analytics_destination_type = each.value.log_analytics_destination_type
+  storage_account_id             = each.value.storage_account_resource_id
+  partner_solution_id            = each.value.marketplace_partner_resource_id
 
   dynamic "enabled_log" {
     for_each = each.value.log_categories != null ? each.value.log_categories : []
@@ -59,6 +65,9 @@ resource "azurerm_monitor_diagnostic_setting" "queue" {
   eventhub_authorization_rule_id = each.value.event_hub_authorization_rule_resource_id
   eventhub_name                  = each.value.event_hub_name
   log_analytics_workspace_id     = each.value.workspace_resource_id
+  log_analytics_destination_type = each.value.log_analytics_destination_type
+  storage_account_id             = each.value.storage_account_resource_id
+  partner_solution_id            = each.value.marketplace_partner_resource_id
 
   dynamic "enabled_log" {
     for_each = each.value.log_categories != null ? each.value.log_categories : []
@@ -91,6 +100,9 @@ resource "azurerm_monitor_diagnostic_setting" "table" {
   eventhub_authorization_rule_id = each.value.event_hub_authorization_rule_resource_id
   eventhub_name                  = each.value.event_hub_name
   log_analytics_workspace_id     = each.value.workspace_resource_id
+  log_analytics_destination_type = each.value.log_analytics_destination_type
+  storage_account_id             = each.value.storage_account_resource_id
+  partner_solution_id            = each.value.marketplace_partner_resource_id
 
   dynamic "enabled_log" {
     for_each = each.value.log_categories != null ? each.value.log_categories : []
@@ -123,6 +135,9 @@ resource "azurerm_monitor_diagnostic_setting" "azure_file" {
   eventhub_authorization_rule_id = each.value.event_hub_authorization_rule_resource_id
   eventhub_name                  = each.value.event_hub_name
   log_analytics_workspace_id     = each.value.workspace_resource_id
+  log_analytics_destination_type = each.value.log_analytics_destination_type
+  storage_account_id             = each.value.storage_account_resource_id
+  partner_solution_id            = each.value.marketplace_partner_resource_id
 
   dynamic "enabled_log" {
     for_each = each.value.log_categories != null ? each.value.log_categories : []
@@ -146,6 +161,3 @@ resource "azurerm_monitor_diagnostic_setting" "azure_file" {
     }
   }
 }
-
-
-
