1d7662421c604fc1bffae488c2329aab3a84b3b8
chinthakaru <chinthakaru@gmail.com>
2026-04-29 19:11:11 +0530

    Pre-Commit

--- examples/role_assignments/main.tf
+++ examples/role_assignments/main.tf
@@ -149,7 +149,7 @@ module "this" {
     ip_rules                   = [try(module.public_ip[0].public_ip, var.bypass_ip_cidr)]
     virtual_network_subnet_ids = toset([azurerm_subnet.private.id])
   }
-  public_network_access_enabled = true
+  public_network_access_enabled = false
   queues = {
     queue0 = {
       name = "queue-${random_string.this.result}-0"
